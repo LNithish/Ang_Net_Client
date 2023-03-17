dd0f74631ae62cf65571299e57467d2e80d977ed
LNithish <nithishkumarvip@gmail.com>
2023-03-17 23:03:07 +0530

    NavBar,HttpClient,Observables,product interface

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Product } from './models/product';
+import { Pagination } from './models/pagination';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+//oninitialization hook is a lifecycle of angular, To use that we have to implement the class with OnInit
+export class AppComponent implements OnInit{
   title = 'NKL';
+  //when strict mode is enabled every property will give an error if it's vale not defined or initialized
+  products:Product[]=[];
+  constructor(private http:HttpClient)
+  {
+  }
+  ngOnInit(): void {
+    //to observe an observable we have to subscribe to it
+    //Specified observable type as Pagination<Product[]>, it is called type safety(benefits of typescript)
+    this.http.get<Pagination<Product[]>>("https://localhost:5001/api/products").subscribe({
+      //items in response is coming from Pagination model
+      next:response=>this.products=response.items,//what to do next
+      error:error=>console.log(error),//what to do if there is an error
+      complete:()=>
+      {
+        //open curly brackets to add multiple lines
+        console.log("Request completed"),
+        console.log("Code works perfectly")
+      }
+    })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    //adding http mudule to access API and get data, need to import httpclient 
+    HttpClientModule
   ],
   providers: [],
   //During angular app starts below component will be started/bootstraped
